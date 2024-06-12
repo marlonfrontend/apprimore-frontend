79ef698f3a9e6ac9e14485fbd140e2244ddb7e23
Marlon Felix <marlonsantos@kirvano.com>
2024-06-12 16:38:21 -0300

    button component using material ui

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,3 +1,17 @@
-export const Button = () => {
-  return <></>
+import { Button as TButton, IconButton as TIconButton } from '@mui/material'
+import { ButtonProps, IconButtonProps } from './Button.types'
+import { PropsWithChildren } from 'react'
+
+export const Button = ({
+  children,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  return <TButton {...props}>{children}</TButton>
+}
+
+export const IconButton = ({
+  children,
+  ...props
+}: PropsWithChildren<IconButtonProps>) => {
+  return <TIconButton {...props}>{children}</TIconButton>
 }
---
+++ src/components/ui/Button/Button.types.ts
@@ -0,0 +1,8 @@
+import {
+  ButtonProps as TButtonProps,
+  IconButtonProps as TIconButtonProps,
+} from '@mui/material'
+
+export type ButtonProps = {} & TButtonProps
+
+export type IconButtonProps = {} & TIconButtonProps
